92d8039fb6beff4d1e2b914d3fb0292cf1e53982
soundOfff <tomas@polluxcoop.com>
2023-09-15 12:56:00 -0400

    Initial commit

---
+++ test/input-management.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as InputManagement from '../lib/input-management-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/input-management-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new InputManagement.InputManagementStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
